ec76a8f2de41ec06098cbfe7d680f99506b2270c
Eduardo Trassi <edu.tyrka@gmail.com>
2026-03-20 10:16:18 -0300

    folder rename

--- exercicios 2 (conditional statements)/exercicio2.py
+++
@@ -1,8 +0,0 @@
-numero = float(input("digite um número: "))
-
-if 10 <= numero <= 50:
-    print("o número esta entre 10 e 50")
-elif numero < 10:
-    print("o número é menor que 10")
-else:
-    print("o número é maior que 50")
--- exercicios 2 (conditional statements)/exercicio3.py
+++
@@ -1,6 +0,0 @@
-ano = int(input("digite um ano: "))
-
-if ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0):
-    print("o ano é bisexto!")
-else:
-    print("o ano não é bisexto!")
